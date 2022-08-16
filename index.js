32142caed0cd4942b90af1549a061f6a6568023d
Sharma Ketan <sharmaketann@gmail.com>
2022-08-16 18:41:15 +0530

    playing with express

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  const person = {
+    name: 'Ketan',
+    isAwesome: true,
+  }
+  res.json(person)
+})
+
+app.get('/awesome-generator', (req, res) => {
+  const { name, isAwesome } = req.query
+  res.send(`${name} is ${JSON.parse(isAwesome) ? 'really' : 'not'} awesome`)
+})
+// Input : http://localhost:8080/awesome-generator?name=%F0%9F%98%8A&isAwesome=false
+// Output : 😊 is not awesome
+
+const port = 8080
+
+app.listen(port, () => {
+  console.log(`Express listening at https://localhost:${port}`)
+})
